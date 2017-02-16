038482c0a60305d89ce29dc2e897d8e8d9ddfa4a
capacitypp <aoi0x4c4f5645@gmail.com>
2017-02-16 14:30:04 +0900

    use proper macro

--- make.sh
+++ make.sh
@@ -1,8 +1,7 @@
 compiler=$1
-cc=$2
 bash -x ~/jenkins-scripts/commit-status.sh $compiler pending pending
 make clean
-make CC=$2 $3
+make CXX=$2 $3
 if [ $? -ne 0 ]; then
 	bash -x ~/jenkins-scripts/commit-status.sh $compiler error error
 	exit 1
