512f1637031814a8b9342a4ccd91a1e12920894c
capacitypp <aoi0x4c4f5645@gmail.com>
2017-02-16 16:28:10 +0900

    add a title to build log

--- make.sh
+++ make.sh
@@ -1,7 +1,8 @@
 compiler=$1
 bash -x ~/jenkins-scripts/commit-status.sh $compiler pending pending
 make clean
-make CXX=$2 $3 > ~/output.txt 2>&1
+echo "#### $compiler" > ~/output.txt
+make CXX=$2 $3 >> ~/output.txt 2>&1
 if [ $? -ne 0 ]; then
 	bash -x ~/jenkins-scripts/commit-status.sh $compiler error error
 	bash -x ~/jenkins-scripts/commit-comment.sh ~/output.txt
