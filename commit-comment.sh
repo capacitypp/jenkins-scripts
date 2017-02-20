ce6012afd34c2dd110912f0856b85ff677cff9de
capacitypp <aoi0x4c4f5645@gmail.com>
2017-02-20 10:29:32 +0900

    send a comment directly

--- commit-comment.sh
+++ commit-comment.sh
@@ -3,5 +3,5 @@ sha=$ghprbActualCommit
 echo "sha : "$sha
 curl -X POST -H "Authorization: token $(cat ~/jenkins-scripts/access_token.txt)" \
 	https://api.github.com/repos/$ghprbGhRepository/commits/$sha/comments -d "{\
-		\"body\": \"$(awk -F\n -v ORS='\\n    ' '{print}' $1)\" \
+		\"body\": \"$(cat $1)\" \
 	}"
--- make.sh
+++ make.sh
@@ -4,8 +4,9 @@ make clean
 echo "#### $compiler" > ~/output.txt
 make $2 >> ~/output.txt 2>&1
 if [ $? -ne 0 ]; then
+	awk -F\n -v ORS='\\n    ' '{print}' ~/output.txt > ~/output2.txt
 	bash -x ~/jenkins-scripts/commit-status.sh $compiler error error
-	bash -x ~/jenkins-scripts/commit-comment.sh ~/output.txt
+	bash -x ~/jenkins-scripts/commit-comment.sh ~/output2.txt
 	exit 1
 fi
 bash -x ~/jenkins-scripts/commit-status.sh $compiler success success
